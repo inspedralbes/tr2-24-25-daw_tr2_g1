4956453efd9be1cc60551d3f866baf9b3188132a
a24amioulabi <a24amioulabi@inspedralbes.cat>
2025-12-17 10:42:52 +0100

    base de dades creada i amb una prova de si funciona, i ara funciona perfectament.

---
+++ db/crear.sql
@@ -0,0 +1,63 @@
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+USE `db_plapi_dev`;
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `alumnes`;
+CREATE TABLE `alumnes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `cognom` varchar(100) NOT NULL,
+  `ralc` varchar(20) NOT NULL,
+  `dni` varchar(15) NOT NULL,
+  `data_naixement` date NOT NULL,
+  `centre_procedencia_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `centre_procedencia_id` (`centre_procedencia_id`),
+  CONSTRAINT `alumnes_ibfk_1` FOREIGN KEY (`centre_procedencia_id`) REFERENCES `centres` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `centres`;
+CREATE TABLE `centres` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `codi` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `pis`;
+CREATE TABLE `pis` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `alumne_id` int NOT NULL,
+  `professor_id` int NOT NULL,
+  `estat` varchar(20) NOT NULL,
+  `ruta_pdf` varchar(255) NOT NULL,
+  `dades_ia` longtext NOT NULL,
+  `data_creacio` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `alumne_id` (`alumne_id`),
+  KEY `professor_id` (`professor_id`),
+  CONSTRAINT `pis_ibfk_1` FOREIGN KEY (`alumne_id`) REFERENCES `alumnes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `pis_ibfk_2` FOREIGN KEY (`professor_id`) REFERENCES `professors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `professors`;
+CREATE TABLE `professors` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `centre_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `centre_id` (`centre_id`),
+  CONSTRAINT `professors_ibfk_1` FOREIGN KEY (`centre_id`) REFERENCES `centres` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
