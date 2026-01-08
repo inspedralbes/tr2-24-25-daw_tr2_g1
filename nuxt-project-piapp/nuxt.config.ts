6fde6cad84a40c54358d94091e0da14857380981
a24jauhurgon <a24jauhurgon@inspedralbes.cat>
2026-01-08 10:49:30 +0100

    Implementada tabla de búsqueda y página de detalle de estudiante

---
+++ nuxt-project-piapp/nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+
+  modules: ['@nuxt/ui'],
+
+  // Add this section to disable Server-Side Rendering (SSR) for the pdf reader page
+  routeRules: {
+    // Assuming your file is named 'pdfreader.vue' inside the 'pages' folder
+    "/pdfreader": { ssr: false },
+  },
+
+  // Optional: If you run into build issues later with pdfjs-dist,
+  // you might need to explicitly transpile it, but try without this first.
+  build: {
+    transpile: ["pdfjs-dist"],
+  },
+});
