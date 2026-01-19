77d0a99b6afa3c048241563c6639b23d54e6e260
BrianBriones <a24bribriera@inspedralbes.cat>
2026-01-19 09:30:54 +0100

    sincronitzacio db

--- back/models.js
+++ back/models.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD,
-    database: process.env.MYSQL_DATABASE || 'db_plapi_dev',
+    database: process.env.DB_NAME || process.env.MYSQL_DATABASE || 'db_plapi_dev',
     waitForConnections: true,
     connectionLimit: 10,
     maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
