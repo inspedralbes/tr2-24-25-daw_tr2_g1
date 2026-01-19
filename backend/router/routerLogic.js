7377886aa4a251cff7a27fcc83db8db4ec78ed93
A24Ole <a24oleproyat@inspedralbes.cat>
2026-01-19 11:05:23 +0100

    create y insertar

--- backend/router/routerLogic.js
+++ backend/router/routerLogic.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { pool } from "../api/db.js";
 import { loginCenter } from "../api/login/loginCenter.js"; // LOGIN CENTRO
 import { createStudent } from "../api/create/createStudent.js"; // CREAR ALUMNO
+import { createStudentPI } from "../api/create/createStudentPI.js"; // CREAR ALUMNO PI
 
 const router = Router();
 
@@ -30,7 +31,7 @@ async function getByEmailCenter(req, res) {
   const centerEmail = req.params.email_centre;
   const [result] = await pool.query(
     "SELECT * FROM centres WHERE email_centre = ?",
-    [centerEmail]
+    [centerEmail],
   );
   res.json(result);
 }
@@ -41,6 +42,8 @@ router.post("/login", loginCenter);
 router.get("/alumnes", getAllStudent);
 router.post("/alumne", createStudent);
 router.get("/alumne/:ralc", getByRalcStudent);
+
+router.post("/alumne/plan_individualitzat", createStudentPI);
 
 router.get("/centres", getAllCenter);
 router.get("/centre/:email_centre", getByEmailCenter);
